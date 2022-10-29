af2b9403057a6b20b41dc0d79bafe3b9cf4597ae
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-28 21:11:59 -0400

    feat: add player ts interface & create session arr

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
 import { Match } from './interfaces/match.interface';
+import { Player } from './interfaces/player.interface';
 
 @Injectable()
 export class EventsService {
-    waiting_queue = [];
+    waiting_queue: Player[] = [];
+    sessions: Player[] = [];
     matches: Match[] = [];
     
     getQueue(): Array<object> {return this.waiting_queue}
