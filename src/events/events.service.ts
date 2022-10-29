20579d6f144bd679ff90dcabc672f0921580cd6e
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-29 03:49:54 -0400

    feat: import, use session type for getter, setter

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
+
+import { Session } from './interfaces/session.interface';
 import { Match } from './interfaces/match.interface';
 import { Player } from './interfaces/player.interface';
 
 @Injectable()
 export class EventsService {
     waiting_queue: Player[] = [];
-    sessions: Player[] = [];
+    sessions: Session[] = [];
     matches: Match[] = [];
 
     // Setter
@@ -16,13 +18,20 @@ export class EventsService {
         return new_length > 0 ? 'added to queue' : 'not added to queue'
     }
     addToMatches() { }
-    addToSessions(player: Player): any {
-        this.sessions.push(player);
-    }
+    addToSessions(session: Session): void { this.sessions.push(session) }
 
     // Getter
 
     getQueue(): Array<Player> { return this.waiting_queue }
     getMatches(): Array<Match> { return this.matches }
-    getSessions(): Array<Player> { return this.sessions }
+    getSessions(): Array<Session> { return this.sessions }
+    getPrintableSessions(): Array<object> {
+        return this.sessions.map(session => ({
+            socket_id: session.socket_id,
+            session_id: session.session_id,
+            timeout: session.timeout === null ? null : {
+                purpose: `this timeout is counting down to delete this session since the associated user has disconnected`
+            }
+        }))
+    }
 }
