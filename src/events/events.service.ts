5276857a33a5a3f785e8235fccb23100c5087d48
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-28 21:14:21 -0400

    feat: add setter and getter for server variables
    Add setter and getter for queue, matches, and sessions

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -8,7 +8,21 @@ export class EventsService {
     waiting_queue: Player[] = [];
     sessions: Player[] = [];
     matches: Match[] = [];
-    
-    getQueue(): Array<object> {return this.waiting_queue}
-    getMatches(): Array<object> {return this.matches}
+
+    // Setter
+
+    addToQueue(player: Player): string {
+        let new_length = this.waiting_queue.push(player)
+        return new_length > 0 ? 'added to queue' : 'not added to queue'
+    }
+    addToMatches() { }
+    addToSessions(player: Player): any {
+        this.sessions.push(player);
+    }
+
+    // Getter
+
+    getQueue(): Array<Player> { return this.waiting_queue }
+    getMatches(): Array<Match> { return this.matches }
+    getSessions(): Array<Player> { return this.sessions }
 }
