ea8bc986b83f8236cf0c14f5bed7ccf54af7be5c
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-11-02 01:37:50 -0400

    feat: create cell interface

---
+++ src/events/interfaces/cell.interface.ts
@@ -0,0 +1,9 @@
+export interface Cell {
+    index: number | null,
+    row: number | null,
+    col: number | null,
+    coordinate: string | null,
+    piece:  'pawn' | 'rook' | 'bishop' | 'knight' | 'queen' | 'king' | 'none' | null,
+    pieceColor: string | null,
+    cellColor: string | null,
+}
