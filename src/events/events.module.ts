c3c80b9e6e1a17586447631fe99be7c6dab5289d
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-11-02 01:40:27 -0400

    refactor: import and use board service

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
 import { EventsService } from './events.service';
+import { BoardService } from './board.service';
 
 @Module({
-  providers: [EventsGateway, EventsService],
+  providers: [EventsGateway, EventsService, BoardService],
 
 })
 export class EventsModule {}
