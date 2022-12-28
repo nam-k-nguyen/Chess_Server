63336185b21610e49ede07330480381e15db206f
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-12-27 21:12:15 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const options = {
+    cors: true,
+    interface: 'eth0'
+  }
+  const app = await NestFactory.create(AppModule, options);
   app.enableCors();
 
   // Uncomment these lines to use the Redis adapter:
